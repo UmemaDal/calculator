8f7bb234c4d05f51d345b3539eadade0ac22e4f4
UmemaDal <115360897+UmemaDal@users.noreply.github.com>
2022-12-28 00:58:05 -0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,37 @@
+import inquirer from "inquirer";
+const answers = await inquirer.prompt([
+    {
+        type: "number",
+        name: "numberOne",
+        message: "kindly enter your first no:"
+    },
+    {
+        type: "number",
+        name: "numberTwo",
+        message: "kindly enter your second no:"
+    },
+    {
+        type: "list",
+        name: "operator",
+        choices: ["*", "+", "-", "/"],
+        message: "select operator:"
+    },
+]);
+const { numberOne, numberTwo, operator } = answers;
+if (numberOne && numberTwo && operator) {
+    let result = 0;
+    if (operator === "+") {
+        result = numberOne + numberTwo;
+    }
+    else if (operator === "-") {
+        result = numberOne - numberTwo;
+    }
+    else if (operator === "/") {
+        result = numberOne / numberTwo;
+    }
+    else if (operator === "*") {
+        result = numberOne * numberTwo;
+    }
+    console.log("your result is :", result);
+    console.log("kindly enter valid input");
+}
---
+++ index.ts
@@ -0,0 +1,36 @@
+import inquirer from "inquirer";
+const answers = await inquirer.prompt([
+    {
+        type: "number",
+        name: "numberOne",
+        message: "kindly enter your first no:"
+    },
+    {
+        type: "number",
+        name: "numberTwo",
+        message: "kindly enter your second no:"
+    },
+    {
+        type: "list",
+        name: "operator",
+        choices: ["*", "+", "-", "/"],
+        message: "select operator:"
+    },
+]);
+
+const {numberOne,numberTwo,operator}=answers;
+if(numberOne && numberTwo && operator){
+      let result:number=0;
+      if(operator==="+"){
+        result=numberOne+numberTwo
+      }else if (operator==="-"){ 
+        result=numberOne-numberTwo
+          }else    if(operator==="/"){
+                result= numberOne/numberTwo
+      }else if (operator==="*"){
+        result=numberOne*numberTwo
+}
+
+console.log("your result is :",result)
+    console.log("kindly enter valid input")
+}
